4e31ce9f797db24eed1099431ba3cd5c16e066a7
Mathias Brandt <mathias@my4.io>
2018-09-27 09:43:37 +0200

    animation optimizations

---
+++ PaymentsHistory/app/src/main/java/com/mathiasbrandt/paymentshistory/AnimationEndListener.kt
@@ -0,0 +1,9 @@
+package com.mathiasbrandt.paymentshistory
+
+import android.animation.Animator
+
+abstract class AnimationEndListener: Animator.AnimatorListener  {
+    override fun onAnimationRepeat(p0: Animator?) {}
+    override fun onAnimationCancel(p0: Animator?) {}
+    override fun onAnimationStart(p0: Animator?) {}
+}
--- PaymentsHistory/app/src/main/java/com/mathiasbrandt/paymentshistory/OverviewActivity.kt
+++ PaymentsHistory/app/src/main/java/com/mathiasbrandt/paymentshistory/OverviewActivity.kt
@@ -7,7 +7,6 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_overview.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
@@ -16,11 +15,24 @@ class OverviewActivity : AppCompatActivity(), AnkoLogger {
     private val viewModelFactory = Injector.get().viewModelFactory()
     private lateinit var overviewVM: OverviewViewModel
 
+    // replace "elevation" with "translationZ"? (set default elevation=10 and translationZ=0 first)
+    private lateinit var headerAnimator: ObjectAnimator
     private var isAnimatingList = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_overview)
+        supportActionBar?.let { actionBar ->
+            headerAnimator = ObjectAnimator.ofFloat(actionBar, "elevation", 10F).apply {
+                duration = 200
+                addListener(object : AnimationEndListener() {
+                    override fun onAnimationEnd(p0: Animator?) {
+                        isAnimatingList = false
+                    }
+                })
+                setAutoCancel(false)
+            }
+        }
 
         overviewVM = ViewModelProviders.of(this, viewModelFactory).get(OverviewViewModel::class.java)
         overviewVM.getLivePayments().observe(this, Observer { payments -> payments?.let { paymentListUpdated(payments) } })
@@ -29,44 +41,16 @@ class OverviewActivity : AppCompatActivity(), AnkoLogger {
         activity_overview_list.adapter = adapter
         activity_overview_list.overScrollMode = View.OVER_SCROLL_NEVER
 
-        activity_overview_list.setOnScrollChangeListener(object: View.OnScrollChangeListener {
-            override fun onScrollChange(p0: View?, p1: Int, p2: Int, p3: Int, p4: Int) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-        })
-
         activity_overview_list.setOnScrollChangeListener { _, _, _, _, _ ->
-            info("##### triggered!")
             val canScrollUp = activity_overview_list.canScrollVertically(-1)
 
-            if(!isAnimatingList && canScrollUp) {
-                isAnimatingList = true
-                supportActionBar?.let { actionBar ->
-                    // replace "elevation" with "translationZ"? (set default elevation=10 and translationZ=0 first)
-                    ObjectAnimator.ofFloat(actionBar, "elevation", 10F).apply {
-                        duration = 200
-                        addListener(object: Animator.AnimatorListener {
-                            override fun onAnimationRepeat(p0: Animator?) {}
-
-                            override fun onAnimationEnd(p0: Animator?) {
-                                isAnimatingList = false
-                            }
-
-                            override fun onAnimationCancel(p0: Animator?) {}
-
-                            override fun onAnimationStart(p0: Animator?) {}
-
-                        })
-                        start()
-                    }
-                }
-            } else if(!canScrollUp) {
-                supportActionBar?.let { actionBar ->
-                    ObjectAnimator.ofFloat(actionBar, "elevation", 0F).apply {
-                        duration = 200
-                        start()
-                    }
+            supportActionBar?.let { actionBar ->
+                if (!isAnimatingList && canScrollUp && actionBar.elevation == 0F) {
+                    isAnimatingList = true
+                    headerAnimator.start()
+                } else if (!isAnimatingList && !canScrollUp && actionBar.elevation > 0F) {
+                    isAnimatingList = true
+                    headerAnimator.reverse()
                 }
             }
 
